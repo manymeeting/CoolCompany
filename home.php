ffbec6e2fb1a4f0626a72e0270eccaf35151aae1
Mokuten OH <mokuteno@gmail.com>
2017-10-25 00:10:24 -0700

    update content and styles of home page

--- home.php
+++ home.php
@@ -30,7 +30,7 @@
   
   <div id="home_banner" class="row">
     <div class="section">
-      <img src="./images/stars.svg" class="svg" align="middle">
+      <img src="./images/stars.svg" class="svg">
       <h1>FLY ME TO THE MOON</h1>
       
       <article>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate </article>
@@ -40,7 +40,7 @@
   <div id="home_content" class="row">
     <div class="padded">
       <div id="about-section" class="col-1-3">
-        <h2>About Us</h2>
+        <h3>About Us</h3>
         <article>
           <?php echo $title; ?> was established in AC2020. Today, we have 1000+ spaceships and 9000+ rockets that are available from all over the world.
         </article>
@@ -48,7 +48,7 @@
       </div>
       
       <div id="products-section" class="col-1-3">
-        <h2>Products</h2>
+        <h3>Products</h3>
         <article>
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         </article>
@@ -56,7 +56,7 @@
       </div>
       
       <div id="news-section" class="col-1-3">
-        <h2>News</h2>
+        <h3>News</h3>
         <article>
           dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irur
         </article>
@@ -65,10 +65,13 @@
     </div>
   </div>
   
-  <div id="home_footer" class="row footer">
-    <h2>Contacts</h2>
-    <ul id = "contacts-list">
-    </ul>
+  <div id="home_contacts" class="row">
+    <h2>Need Help?</h2>
+    <div id="contacts-section">
+      <div id="contacts-list" class="autogrid">
+      </div>
+    </div>
+    
     <script>
       // load contacts information from file
       let contacts = [];
@@ -80,13 +83,25 @@
         ?>;
       
       // initialize contacts list
-      let contactsHTML = ""
-      contacts.forEach(function(contact) {
-        contactsHTML += "<li>" + contact.name + ": " + contact.email + ", tel: " + contact.tel + "</li>";
-      });
+      let contactsHTML = "";
+      for(let i = 0; i < contacts.length; i++)
+      {
+        contactsHTML +=
+        '<div class="col">' +
+          '<img src="./images/chat.png" class="size-forty">' +
+          '<strong>' + contacts[i].name + '</strong>' +
+          '<article>' + contacts[i].intro + '</article>' +
+          '<p><a href="#" class="btn small solid round">' + contacts[i].email + '</a></p>' +
+        '</div>';
+      }
+      
       $("#contacts-list").html(contactsHTML);
       
     </script>
+  </div>
+  
+  <div id="home_footer" class="row footer">
+    <p>© 2017 Light Year Trael, Inc.</p>
   </div>
 </body>
 </html>
