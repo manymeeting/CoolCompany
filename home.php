e33244ef4b824b450b83eb2e251613e868a5c65a
Mokuten OH <mokuteno@gmail.com>
2017-10-24 21:43:35 -0700

    add more content and styles to home page

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
 <head>
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" type="text/css" href="./css/vital.css">
+    <link rel="stylesheet" type="text/css" href="./css/common.css">
     <link rel="stylesheet" type="text/css" href="./css/main.css">
     <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="./js/common.js"></script>
@@ -18,9 +19,10 @@
             <img src="./images/logo.svg" alt="logo">
           </a>
           <ul class="menu">
-              <li><a href="./home.php">Home</a></li>
-              <li><a href="./about.php">About</a></li>
-              <li><a href="./news.php">News</a></li>
+            <li><a href="./news.php">News</a></li>
+            <li><a href="./products.php">Products</a></li>
+            <li><a href="./about.php">About</a></li>
+            <li><a href="./home.php">Home</a></li>
           </ul>
         </nav>
       </div>
@@ -35,43 +37,36 @@
     </div>
   </div>
   
+  <div id="home_content" class="row">
+    <div class="padded">
+      <div id="about-section" class="col-1-3">
+        <h2>About Us</h2>
+        <article>
+          <?php echo $title; ?> was established in AC2020. Today, we have 1000+ spaceships and 9000+ rockets that are available from all over the world.
+        </article>
+        <p class="section-footer-btn"><a href="#" class="btn small round blue">more...</a></p>
+      </div>
+      
+      <div id="products-section" class="col-1-3">
+        <h2>Products</h2>
+        <article>
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        </article>
+        <p class="section-footer-btn"><a href="#" class="btn small round blue">more...</a></p>
+      </div>
+      
+      <div id="news-section" class="col-1-3">
+        <h2>News</h2>
+        <article>
+          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irur
+        </article>
+        <p class="section-footer-btn"><a href="#" class="btn small round blue">more...</a></p>
+      </div>
+    </div>
+  </div>
   
-  <section id="home-section">
-    <h3>Home</h3>
-    <main>
-      We are an amazing group! We bring you to any planet in the universe!
-    </main>
-  </section>
-  
-  <section id="about-section">
-    <h3>About</h3>
-    <article>
-      <?php echo $title; ?> was established in AC2020. Today, we have 1000+ spaceships and 9000+ rockets that are available from all over the world.
-    </article>
-  </section>
-  
-  <section id="products-section">
-    <h3>Products</h3>
-    <p>We offer exciting products!</p>
-    <ol>
-      <li>Fly me to me Moon</li>
-      <li>Solar System Safari</li>
-      <li>Inside Andromeda Galaxy</li>
-    </ol>
-  </section>
-  
-  <section id="news-section">
-    <h3>News</h3>
-    <p>Amazing things happen everyday!</p>
-    <ol>
-      <li>Spruce Goose safely came back from Mars!</li>
-      <li>Of Course I Still Love You launched successfully!</li>
-      <li>100 Anniversary Next Month!</li>
-    </ol>
-  </section>
-  
-  <section id="contacts-section">
-    <h3>Contacts</h3>
+  <div id="home_footer" class="row footer">
+    <h2>Contacts</h2>
     <ul id = "contacts-list">
     </ul>
     <script>
@@ -92,6 +87,6 @@
       $("#contacts-list").html(contactsHTML);
       
     </script>
-  </section>
+  </div>
 </body>
 </html>
