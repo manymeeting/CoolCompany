d8980af5744781db4a186890e4cd3034d4e8e93c
Mokuten OH <mokuteno@gmail.com>
2017-10-26 12:50:30 -0700

    initialize login page

---
+++ login.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" type="text/css" href="./css/vital.css">
+    <link rel="stylesheet" type="text/css" href="./css/common.css">
+    <link rel="stylesheet" type="text/css" href="./css/login.css">
+    <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./js/common.js"></script>
+  </head>
+
+  <body>
+    
+    <div class="row">
+      <div id="login_content" class="section">
+        <img src="./images/galaxy.svg" class="svg">
+
+        <div class="autogrid full-width-forms">
+          <div class="col-1-4 space"></div>
+          <div class="col-1-2">
+            <h3 class="center">Sign In</h3>
+            <div class="box bg-white no-first-last">
+              <p>
+                <input placeholder="Email" type="email">
+                <input placeholder="Password" type="password">
+              </p>
+              <p><a href="#" class="btn gray-dark solid">Go</a></p>
+              <hr>
+              <div class="col-1-2">
+                  <a href="#" class="btn gray-medium no-outline small">Sign Up</a>
+              </div>
+              <div class="col-1-2"><a href="#" class="btn gray-medium no-outline small">Forgot Password?</a></div>
+              <div class="clear"></div>
+            </div>
+          </div>
+          <div class="col-1-4 space"></div>
+          <div class="clear"></div>
+        </div>
+      </div>
+    </div>
+    
+  </body>
+</html>
