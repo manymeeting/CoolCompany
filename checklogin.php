91946195c7f51a252f74cc6b4860f21e47bbaa0f
Mokuten OH <mokuteno@gmail.com>
2017-10-27 22:50:59 -0700

    add simple pwd check logic

--- checklogin.php
+++ checklogin.php
@@ -1,10 +1,35 @@
 <?php
-
-// Define $myusername and $mypassword
+$ADMIN_PWD_FILENAME = "admin_pwd";
+// define $myusername and $mypassword
 $username = $_POST['username'];
 $password = $_POST['password'];
-echo $username;
-echo $password;
 
+
+// get saved admin pwd
+$adminPwd = fetchAdminPwd($ADMIN_PWD_FILENAME);
+if(password_verify($password, $adminPwd))
+{
+  $_SESSION['valid'] = true;
+  header("Location: home.php");
+  exit();
+}
+else
+{
+  header("Location: login.php");
+  exit();
+}
+
+
+/*
+* Fetch hashed password of admin from a file
+* @return {String} pwd
+*/
+function fetchAdminPwd($fileName)
+{
+  $file = fopen($fileName, "r") or die("Unable to open file");
+  $pwd = fgets($file);
+  fclose($file);
+  return $pwd;
+};
 
 ?>
