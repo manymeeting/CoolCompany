5544ae88949bd71a6623f55bd9a3a0272c8f554c
Mokuten OH <mokuteno@gmail.com>
2017-10-28 14:29:07 -0700

    add username check for login

--- checklogin.php
+++ checklogin.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 $ADMIN_PWD_FILENAME = "admin_pwd";
-// define $myusername and $mypassword
+$ADMIN_ID = "admin";
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 // get saved admin pwd
 $adminPwd = fetchAdminPwd($ADMIN_PWD_FILENAME);
-if(password_verify($password, $adminPwd))
+if(password_verify($password, $adminPwd) && ($username === $ADMIN_ID))
 {
   $_SESSION['valid'] = true;
   header("Location: home.php");
@@ -18,7 +19,6 @@ else
   header("Location: login.php");
   exit();
 }
-
 
 /*
 * Fetch hashed password of admin from a file
