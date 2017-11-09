ed36d5ee54dc94846bfdceae9d4670cb82977518
Mokuten OH <mokuteno@gmail.com>
2017-11-08 16:04:07 -0800

    add new font

--- general_header.php
+++ general_header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="./css/vital.css">
     <link rel="stylesheet" type="text/css" href="./css/common.css">
     <link rel="stylesheet" type="text/css" href="./css/table.css">
+    <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
     <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="./js/common.js"></script>
 <!-- </head> -->
