6a6ce945ace63265f4984a426953d61cb8a6937a
Mokuten OH <mokuteno@gmail.com>
2017-11-10 21:41:06 -0800

    add js.cookie API

--- common_head.php
+++ common_head.php
@@ -10,5 +10,6 @@
     <link rel="stylesheet" type="text/css" href="./css/coolCompany.css">
     <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
     <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./js/js.cookie.js"></script>
     <script type="text/javascript" src="./js/common.js"></script>
 <!-- </head> -->
