872516cbeb091fd2ea7bcabef1f90c1c09fff5a5
Mokuten OH <mokuteno@gmail.com>
2017-11-25 02:31:55 -0800

    add a new utilities include file

---
+++ includes/functions.php
@@ -0,0 +1,18 @@
+<?php
+//Class Autoloader
+spl_autoload_register(function ($className) {
+
+    $className = strtolower($className);
+    $path = "includes/{$className}.php";
+
+    if (file_exists($path)) {
+
+        require_once($path);
+
+    } else {
+
+        die("The file {$className}.php could not be found.");
+
+    }
+});
+?>
