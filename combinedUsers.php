8a4bcb0067e4d38ca22f12f3d197423218333022
Mokuten OH <mokuteno@gmail.com>
2017-12-03 15:25:11 -0800

    use curl to get Nancy's users

--- combinedUsers.php
+++ combinedUsers.php
@@ -9,26 +9,40 @@
     require dirname(__FILE__).'/' . 'config.php';
 
     // local users
-    $url = "http://" . HOST_ADDR . "/CoolCompany/api/myusers.php";
+    $url_local = "http://" . HOST_ADDR . "/CoolCompany/api/myusers.php";
 
     $post_data = array ();
 
     $cu = curl_init();
-    curl_setopt($cu, CURLOPT_URL, $url);
+    curl_setopt($cu, CURLOPT_URL, $url_local);
     curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1);
 
     curl_setopt($cu, CURLOPT_POST, 1);
     curl_setopt($cu, CURLOPT_POSTFIELDS, $post_data);
 
     $response_local = curl_exec($cu);
-    print "Local: </br>";
+    print "Local Users: </br>";
     print $response_local;
     print "</br>";
 
+    // Nancy's users
+    $url_nancy = "http://nx168.xyz/userlist.php";
+    curl_setopt($cu, CURLOPT_URL, $url_nancy);
+    $response_nancy = curl_exec($cu);
+    print "Nancy's Users: </br>";
+    print $response_nancy;
+    print "</br>";
+
+    // Ruifeng's users
+
+
+    // Vencci's users
+
+    // Na's users
+
+
     curl_close($cu);
 
-    // Nancy's users
-    
   ?>
 </body>
 </html>
